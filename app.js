eec25134be845d92c300b4e2ea2c6b297346fed7
CodeSniper12 <98008988+codesniper12@users.noreply.github.com>
2025-01-09 13:38:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.log('Service Worker Registered'))
+      .catch((err) => console.log('Service Worker Registration Failed:', err));
+  }
+  
